8de77bdf9ad26a77a87179a083e808fa916f38dc
adamhadiwardoyo <adam.hadiwardoyo@gmail.com>
2025-09-22 13:36:10 +0700

    added analytics layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,15 +1,14 @@
-//NEXT INTERNATIONAL
+// NEXT INTERNATIONAL
 import { NextIntlClientProvider, hasLocale } from 'next-intl'
 import { routing } from '@/i18n/routing';
 import { notFound } from 'next/navigation'
 
-
-//DEFAUL CONFIGURATION
+// DEFAULT CONFIGURATION
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-//COMPONENTS
+// COMPONENTS
 import ContactBubble from "@/components/ContactBubble";
 
 // app/[locale]/layout.tsx atau komponen Swiper kamu
@@ -17,7 +16,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-
+import Script from "next/script";
+import Analytics from "@/components/Analytics"; // kita bikin komponen client
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,16 +48,30 @@ export default async function RootLayout({
   }
 
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang={locale}>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WXH6D3K8XH"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WXH6D3K8XH');
+          `}
+        </Script>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <NextIntlClientProvider>
           {children}
-
           <ContactBubble />
         </NextIntlClientProvider>
 
+        {/* Track navigasi antar route */}
+        <Analytics />
       </body>
     </html>
   );
